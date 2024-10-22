b2a869de3445fa040d095eba1f2af186152cd169
Javokhir Abdullaev <javokhir_abdullaev@epam.com>
2024-10-22 12:12:17 +0500

    fix tests

--- src/test/java/org/folio/rest/tools/utils/ModuleName.java
+++ src/test/java/org/folio/rest/tools/utils/ModuleName.java
@@ -3,9 +3,14 @@ package org.folio.rest.tools.utils;
 public class ModuleName {
 
   private static final String MODULE_NAME = "folio_liquibase_util";
+  private static final String MODULE_VERSION = "1.0.0"; // or the appropriate version
 
   public static String getModuleName() {
     return MODULE_NAME;
+  }
+
+  public static String getModuleVersion() {
+    return MODULE_VERSION;
   }
 
 }
